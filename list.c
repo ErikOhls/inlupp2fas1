bad5307dd562c85ce9798ae8a596b3ee611f133b
Erik <Ohls2nd@gmail.com>
2017-10-04 20:21:25 +0200

    list.c kom inte med

--- list.c
+++ list.c
@@ -1,9 +1,29 @@
 #include <stdbool.h>
 #include "common.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node node_t;
+
+struct node{
+  node_t *next;
+  elem_t elem;
+};
 
 typedef struct list list_t;
 
-/// Creates a new list 
+struct list{
+  node_t *first;
+  node_t *last;
+
+  element_copy_fun copy;
+  element_free_fun free;
+  element_comp_fun compare;
+
+  int size;
+};
+
+/// Creates a new list
 ///
 /// \param copy (may be NULL) a function applied to all elements when stored in the list
 /// \param free (may be NULL) used to free elements in list_delete
@@ -11,11 +31,26 @@ typedef struct list list_t;
 /// \returns: empty list
 list_t *list_new(element_copy_fun copy, element_free_fun free, element_comp_fun compare)
 {
-  
+  list_t *result = calloc(1, sizeof(list_t));
+
+  result->copy = copy;
+  result->free = free;
+  result->compare = compare;
+
+  result->size = 0;
+
+  return result;
 }
 
+node_t *node_new(elem_t elem, node_t *next)
+{
+  node_t *result = calloc(1, sizeof(node_t));
+  result->elem = elem;
+  result->next = next;
+  return result;
+}
 
-/// Inserts a new element at a given index. 
+/// Inserts a new element at a given index.
 ///
 /// If list's copy function is non-NULL, it will be applied to elem and its result
 /// stored in the list. Otherwise, elem will be stored in the list. 
@@ -29,7 +64,46 @@ list_t *list_new(element_copy_fun copy, element_free_fun free, element_comp_fun 
 /// \param elem  the element to be inserted
 void list_insert(list_t *list, int index, elem_t elem)
 {
-  return;
+  if(list->first == NULL)                 // Om tom lista
+    {
+      puts("Inserting in empty list\n");
+      list->first = node_new(list->copy(elem), NULL);
+      list->last = list->first;
+      ++list->size;
+    }
+  else
+    {
+      if(index == 0)                          // Om ny första position
+        {
+          puts("Inserting at first position in list\n");
+          node_t *new_first = node_new(list->copy(elem), list->first);
+          list->first = new_first;
+          ++list->size;
+        }
+      else
+        {
+          node_t **cursor = &(list->first);
+          if(index > list->size)              // Om index > än lista, ändra index
+            {
+              index = list->size-1;
+            }
+          if(index < 0)                       // Om index är negativ, ommvandla till posetiv
+            {
+              index = index + 1 + list->size;
+            }
+          for(int i = 0; i < index; i++)
+            {
+              cursor = &((*cursor)->next);
+            }
+          *cursor = node_new(list->copy(elem), *cursor);
+          if(index == list->size)             // Om ny sista position
+            {
+              puts("inserting in last position of list\n");
+              list->last = list->last->next;
+            }
+          ++list->size;
+        }
+    }
 }
 
 /// Inserts a new element at the end of the list.
@@ -120,7 +194,14 @@ void list_delete(list_t *list, bool delete)
 /// \returns the result of all fun calls, combined with OR (||)
 bool list_apply(list_t *list, elem_apply_fun fun, void *data)
 {
-  return false;
+  // TODO: Fixa så detta funkar som det ska
+  node_t *cursor = list->first;
+  for(int i = 0; i < list->size; i++)
+    {
+      fun(cursor->elem, data);
+      cursor = cursor->next;
+    }
+  return true;
 }
 
 /// Searches for an element in a list
