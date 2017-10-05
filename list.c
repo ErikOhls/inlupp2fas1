63b27327e5c8babd3aa8df1f4884f163ae435985
Erik <Ohls2nd@gmail.com>
2017-10-05 16:31:13 +0200

    pillat lite på list

--- list.c
+++ list.c
@@ -85,7 +85,7 @@ void list_insert(list_t *list, int index, elem_t elem)
           node_t **cursor = &(list->first);
           if(index > list->size)              // Om index > än lista, ändra index
             {
-              index = list->size-1;
+              index = -1;
             }
           if(index < 0)                       // Om index är negativ, ommvandla till posetiv
             {
@@ -109,25 +109,25 @@ void list_insert(list_t *list, int index, elem_t elem)
 /// Inserts a new element at the end of the list.
 ///
 /// If list's copy function is non-NULL, it will be applied to elem and its result
-/// stored in the list. Otherwise, elem will be stored in the list. 
+/// stored in the list. Otherwise, elem will be stored in the list.
 ///
 /// \param list pointer to the list
 /// \param elem the element to be appended
 void list_append(list_t *list, elem_t elem)
 {
-  return;
+  list_insert(list, -1, elem);
 }
 
 /// Inserts a new element at the beginning of the list
 ///
 /// If list's copy function is non-NULL, it will be applied to elem and its result
-/// stored in the list. Otherwise, elem will be stored in the list. 
+/// stored in the list. Otherwise, elem will be stored in the list.
 ///
 /// \param list pointer to the list
 /// \param elem the element to be prepended
 void list_prepend(list_t *list, elem_t elem)
 {
-  return;
+  list_insert(list, 0, elem);
 }
 
 /// Removes an element from a list.
@@ -135,13 +135,26 @@ void list_prepend(list_t *list, elem_t elem)
 /// All indexes are valid. 0 means first element. Negative indexes
 /// count backward and too large negative indexes equal 0. Too
 /// large positive indexes are same as -1.
-/// 
+///
 /// \param list  pointer to the list
 /// \param index the index to be removed
 /// \param delete if true, run list's free function on all elements
 void list_remove(list_t *list, int index, bool delete)
 {
-  return;
+  if(list->size == 0)                       // Tom lista
+    {
+      return;
+    }
+  if(delete)                                // Delete all
+    {
+      //list_apply(TODO: delete_elem function);
+      return;
+    }
+  else                                      // Delete @ index
+    {
+      
+      return;
+    }
 }
 
 /// Returns the element at a given index
@@ -151,19 +164,38 @@ void list_remove(list_t *list, int index, bool delete)
 /// \returns true if index was a valid index
 bool list_get(list_t *list, int index, elem_t *result)
 {
-  return false;
+  if(index < 0 || index > list->size)
+    {
+      puts("index out of bounds\n");
+      return false;
+    }
+  else
+    {
+      puts("index in bounds\n");
+      node_t **cursor = &(list->first);
+      for(int i = 0; i < index; i++)
+        {
+          puts("iterating\n");
+          cursor = &((*cursor)->next);
+        }
+      puts("assigning result\n");
+      result = &((*cursor)->elem);
+      //puts("printing element:\n");
+      printf("elem @ index %d is %d\n", index, result); // Sketchy
+      return true;
+    }
 }
 
 /// A convenience for list_get(list, 0, result)
 bool list_first(list_t *list, elem_t *result)
 {
-  return false;
+  return list_get(list, 0, result);
 }
 
 /// A convenience for list_get(list, -1, result)
 bool list_last(list_t *list, elem_t *result)
 {
-  return false;
+  return list_get(list, -1, result);
 }
 
 /// Returns the length of the list. It is undefined
@@ -174,7 +206,7 @@ bool list_last(list_t *list, elem_t *result)
 /// \returns the length of list
 int list_length(list_t *list)
 {
-  return 0;
+  return list->size;
 }
 
 /// Deletes a list. 
--- tester.c
+++ tester.c
@@ -6,13 +6,13 @@
 
 elem_t copy_func(elem_t elem)
 {
-  //elem_t ret;
-  //elem_t *copy = calloc(1, sizeof(elem_t));
-  //*copy = elem;
   return elem;
 }
 
-void free_func();
+void free_func(elem_t elem)
+{
+  return;
+}
 
 int comp_func(elem_t elem, elem_t elem2)
 {
@@ -33,17 +33,34 @@ bool print_func(elem_t elem, void *data)
   return true;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
   elem_t elem1 = { .i=1 };
   elem_t elem2 = { .i=2 };
   elem_t elem3 = { .i=3 };
+  elem_t elem4 = { .i=4 };
+  elem_t elem5 = { .i=5 };
+  elem_t elem6 = { .i=6 };
+  elem_t elem7 = { .i=7 };
+  elem_t elem8 = { .i=8 };
+  elem_t elem9 = { .i=9 };
 
-  list_t *the_list = list_new(copy_func, NULL, comp_func);
+  list_t *the_list = list_new(copy_func, free_func, comp_func);
 
   list_insert(the_list, 0, elem1);
   list_insert(the_list, 1, elem2);
   list_insert(the_list, 2, elem3);
+  list_insert(the_list, 1, elem4);
+
+  list_prepend(the_list, elem5);
+
+  list_append(the_list, elem6);
+
+  list_insert(the_list, 1000, elem8);
+
+  puts("running list get\n");
+  elem_t *retrieved;
+  list_get(the_list, 7, retrieved);
 
   list_apply(the_list, print_func, NULL);
 
